6e1a947a6957f299bfa285ce12df7050f88805d5
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-10-13 15:39:30 +0100

    y

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from blueprints.activities.routes import activities  # Adjust the import based on your folder structure
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.register_blueprint(activities, url_prefix='/activities')
 
