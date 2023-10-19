1800a816dc7d127ab7fab34519bc3637cd23a152
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-10-19 10:47:13 +0100

    y

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,24 @@
 from flask import Flask
-from blueprints.activities.routes import activities  # Adjust the import based on your folder structure
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# Initialize extensions outside of app context
+db = SQLAlchemy()
+migrate = Migrate()
 
 app = Flask(__name__)
 
+# Configuration settings
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Obsidian1989!@localhost:5432/tefl_app_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Attach extensions to the app
+db.init_app(app)
+migrate.init_app(app, db)
+
+# Now import your models and blueprints AFTER initializing the app and extensions
+from models import User, UserLevel
+from blueprints.activities.routes import activities
 
 app.register_blueprint(activities, url_prefix='/activities')
 
--- backend/database.py
+++
@@ -1,6 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from app import app   # Import the Flask app instance
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
