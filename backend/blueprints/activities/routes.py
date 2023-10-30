d0c0d8c6ff3f324e6253488b6cd4d28767ca41db
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-10-30 16:42:07 +0000

    y

--- backend/blueprints/activities/routes.py
+++ backend/blueprints/activities/routes.py
@@ -2,23 +2,30 @@ from . import activities
 from app import db
 from models import User, UserLevel
 from .levels import get_rating
+from .starter_questions import questions
+from flask import request, jsonify
+
 
 @activities.route('/test_add_user', methods=['POST'])
 def test_add_user():
     # Adding a dummy user
-    user = User(username="dummyUser", email="dummy@user.com")
+    user = User(username="dummyUser30", email="dummy@user30.com")
     db.session.add(user)
     db.session.commit()
 
     # Simulate a quiz evaluation for the dummy user
     dummy_answers = request.json.get('answers')
-    score = sum(question['weight'] for i, question in enumerate(questions) if dummy_answers[i] == question['correct_answer'])
+    score = sum(question['weight'] for i, question in enumerate(questions) if dummy_answers[i] ==
+                question['correct_answer'])
     proficiency_level = get_rating(score)
+
 
     # Assigning a level to the user based on the evaluation
     user_level = UserLevel(level=proficiency_level, user_id=user.id)
     db.session.add(user_level)
+    print(f"UserLevel added to session with level {proficiency_level}.")
     db.session.commit()
+    print("UserLevel committed.")
 
     return jsonify({
         "username": user.username,
