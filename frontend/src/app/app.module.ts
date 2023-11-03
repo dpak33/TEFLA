fad9e8d382ca919d0a9ec8f5a9c1646929a6b153
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-11-03 13:37:35 +0000

    y

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    SignupComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/auth/auth.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent {
+  showSignup = true;
+
+  toggleView() {
+    this.showSignup = !this.showSignup;
+  }
+}
