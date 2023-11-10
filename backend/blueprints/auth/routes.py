cdadd481f2a7b394a1483e9552be52d62d0d9565
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-11-10 14:28:25 +0000

    y

--- backend/blueprints/auth/routes.py
+++ backend/blueprints/auth/routes.py
@@ -1,42 +1,41 @@
 from flask import render_template, redirect, url_for, flash, request
 from werkzeug.security import check_password_hash, generate_password_hash
 from . import auth_bp
-from backend.models import User
-from backend.extensions import db
+from models import User
+from app import db
+
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Example: get data from form
         username = request.form.get('username')
         password = request.form.get('password')
-        # Validate and check user
+
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password_hash, password):
             # User authenticated
-            # Log in the user (e.g., using Flask-Login)
+            flash('Login successful! Welcome back.')
             return redirect(url_for('main.index'))  # Redirect to main page after login
         else:
-            flash('Invalid username or password')
-    # GET or failed POST
+            flash('Invalid username or password', 'error')
     return render_template('auth/login.html')  # Render the login template
+
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        # Example: get data from form
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        # Check if user already exists
-        user = User.query.filter_by(username=username).first()
-        if user:
-            flash('Username already exists')
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists', 'error')
         else:
-            # Create new user and add to database
-            new_user = User(username=username, email=email, password_hash=generate_password_hash(password))
+            new_user = User(username=username, email=email)
+            new_user.set_password(password)  # Use the set_password method to hash the password
             db.session.add(new_user)
             db.session.commit()
+            flash('Registration successful! Please log in.')
             return redirect(url_for('auth.login'))  # Redirect to login page after register
-    # GET or failed POST
-    return render_template('auth/register.html')
+    return render_template('auth/register.html')  # Render the register template
