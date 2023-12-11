09647f8e92fe4ab79e0cb545387e38c6854130af
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-12-11 14:54:20 -0800

    y

--- frontend/src/app/studyroom/travel-beginner/travel-beginner.component.ts
+++ frontend/src/app/studyroom/travel-beginner/travel-beginner.component.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class TravelBeginnerComponent {
   currentPage = 1;
+  transportModes = [
+  { name: 'Car', imageUrl: '../../../assets/car.jpg' },
+  { name: 'Bus', imageUrl: '../../../assets/bus.jpg' },
+  { name: 'Bicycle', imageUrl: '../../../assets/bicycle.jpg' },
+  { name: 'Train', imageUrl: '../../../assets/train.jpg' },
+  { name: 'Motorcycle', imageUrl: '../../../assets/motorcycle.jpg' },
+  { name: 'Boat', imageUrl: '../../../assets/rowboat.jpg' },
+  { name: 'Airplane', imageUrl: '../../../assets/airplane.jpg' },
+  { name: 'Helicopter', imageUrl: '../../../assets/helicopter.jpg' },
+  { name: 'Subway', imageUrl: '../../../assets/subway.jpg' },
+  { name: 'Taxi', imageUrl: '../../../assets/taxi.jpg' },
+  { name: 'Scooter', imageUrl: '../../../assets/scooter.jpg' },
+  { name: 'Truck', imageUrl: '../../../assets/truck.jpg' }
+];
 
   // Function to change page
   changePage(pageNumber: number) {
