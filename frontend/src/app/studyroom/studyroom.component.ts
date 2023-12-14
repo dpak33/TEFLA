2caa2a69ea12ffcc48b4be0367905d10b7743930
dpak33 <105285976+dpak33@users.noreply.github.com>
2023-12-14 10:15:32 -0800

    y

--- frontend/src/app/studyroom/studyroom.component.ts
+++ frontend/src/app/studyroom/studyroom.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, Event as RouterEvent } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-studyroom',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./studyroom.component.css']
 })
 export class StudyroomComponent {
+  isTravelBeginnerActive: boolean = false;
 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+    //below typeguard for ensuring type-safety
+      filter((event: RouterEvent): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+    //Change bool to true if url is travel-beginner as below:
+      this.isTravelBeginnerActive = event.urlAfterRedirects.includes('/travel-beginner');
+    });
+  }
 }
