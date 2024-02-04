eba5de2176bbda41dd4373bfa4fa136b3b4cf78d
dpak33 <105285976+dpak33@users.noreply.github.com>
2024-02-04 11:06:52 -0800

    y

--- frontend/src/app/studyroom/travel-intermediate/travel-intermediate.component.ts
+++ frontend/src/app/studyroom/travel-intermediate/travel-intermediate.component.ts
@@ -7,5 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./travel-intermediate.component.css']
 })
 export class TravelIntermediateComponent {
+  currentPage = 1;
+
+
   constructor(private router: Router) {}
+  startTravelIntermediateQuiz() {
+  this.router.navigate(['/section-quizzes', 'intermediate', 'travel']);
+  }
+
+  //Change page
+  changePage(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
 }
