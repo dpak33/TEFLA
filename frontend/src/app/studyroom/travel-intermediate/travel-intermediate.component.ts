5abd7c89856d621451df78eacb1efe6925becfed
dpak33 <105285976+dpak33@users.noreply.github.com>
2024-02-06 09:33:04 -0800

    y

--- frontend/src/app/studyroom/travel-intermediate/travel-intermediate.component.ts
+++ frontend/src/app/studyroom/travel-intermediate/travel-intermediate.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-travel-intermediate',
   templateUrl: './travel-intermediate.component.html',
-  styleUrls: ['./travel-intermediate.component.css']
+  styleUrls: ['../studyroom.component.css']
 })
 export class TravelIntermediateComponent {
   currentPage = 1;
