b5f4995fbedb34fc3dc0d2c40a9fcd62f55e8308
dpak33 <105285976+dpak33@users.noreply.github.com>
2024-02-06 18:25:14 -0800

    y

--- frontend/src/app/studyroom/greetings-beginner/greetings-beginner.component.ts
+++ frontend/src/app/studyroom/greetings-beginner/greetings-beginner.component.ts
@@ -4,9 +4,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-greetings-beginner',
   templateUrl: './greetings-beginner.component.html',
-  styleUrls: ['./greetings-beginner.component.css']
+  styleUrls: ['../studyroom.component.css']
 })
-
 export class GreetingsBeginnerComponent {
+  currentPage = 1;
+
+
   constructor(private router: Router) {}
+  startGreetingsBeginnerQuiz() {
+  this.router.navigate(['/section-quizzes', 'beginner', 'greetings']);
+  }
+
+  //Change page
+  changePage(pageNumber: number) {
+    this.currentPage = pageNumber;
+  }
 }
